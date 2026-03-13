a50d600b58be3d9ed3cba648916c87ed83700929
Jake <blazingpika7@gmail.com>
2026-03-13 12:28:02 -0500

    Finished "Create, Read, Update, and Delete"

--- ContosoUniversity/obj/Debug/net10.0/ContosoUniversity.AssemblyInfo.cs
+++ ContosoUniversity/obj/Debug/net10.0/ContosoUniversity.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fff466439081374b1e8f4aa7b7c71ce57894d870")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2c937074b160a10ddc073ec207056cead37fd62")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
